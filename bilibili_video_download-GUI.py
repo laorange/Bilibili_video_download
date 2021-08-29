c0d7b7b2c5b2fc594e534dbd05b28a8fc6b4261a
辣橙 <laorange6666@qq.com>
2021-08-29 16:25:35 +0800

    优化ui

--- bilibili_video_download-GUI.py
+++ bilibili_video_download-GUI.py
@@ -1,10 +1,10 @@
 # !/usr/bin/python
 # -*- coding:utf-8 -*-
 # time: 2019/07/02--08:12
-__author__ = 'Henry'
+# __author__ = 'Henry'
 
-# edit time: 2021-08-28
-# edit by laorange
+# edit time: 2021-08-29
+#
 
 """
 项目: B站视频下载 - GUI版本
@@ -13,7 +13,7 @@ __author__ = 'Henry'
 20190702 - 增加视频多线程下载 速度大幅提升
 20190711 - 增加GUI版本,可视化界面,操作更加友好
 
-20210828 - 修复b站接口更换带来的bug & 优化UI
+20210828 - 修复b站接口更换带来的bug & 优化UI  【edit by Github@laorange】
 """
 
 import requests
@@ -33,6 +33,12 @@ from tkinter import StringVar
 
 downloaded_num = 0
 total_amount = 0
+
+
+def initialize():
+    global downloaded_num, total_amount
+    downloaded_num = total_amount = 0
+    confirm['state'] = tk.NORMAL
 
 
 def base_path(path):
@@ -60,6 +66,7 @@ def print_diy(text, print_time=True):
     if print_time:
         output = time_now + "：" + output
     msgbox.insert(tk.END, output)
+    msgbox.see(tk.END)
 
 
 # 访问API地址
@@ -71,7 +78,7 @@ def get_play_list(start_url, cid, quality):
     url_api = 'https://interface.bilibili.com/v2/playurl?%s&sign=%s' % (params, chksum)
     headers = {
         'Referer': start_url,  # 注意加上referer
-        # 'Cookie': 'SESSDATA=aa15d6af%2C1560734457%2Ccc8ca251',
+        # 'Cookie': '',
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
     }
     # print(url_api)
@@ -102,8 +109,9 @@ def process_for_one_page(blocknum, blocksize, totalsize):
 
     # 设置下载进度条
     percent = recv_size / totalsize
-    if percent > 1:
+    if percent >= 1:
         percent = 1
+        initialize()
     percent_str = "%.2f%%" % (percent * 100)
 
     download.coords(fill_line1, (0, 0, percent * 465, 23))
@@ -128,32 +136,14 @@ def process_for_several_pages(blocknum, blocksize, totalsize):
             downloaded_num += 1
 
     total_percentage = (downloaded_num + percent) / total_amount if downloaded_num != total_amount else 1
-
-    # if total_percentage > 0.8:
-    #     print(downloaded_num, total_amount, percent, total_percentage)
+    if total_percentage == 1:
+        initialize()
 
     percent_str = "%.2f%%" % (total_percentage * 100)
 
     download.coords(fill_line1, (0, 0, total_percentage * 465, 23))
     root.update()
     pct.set(percent_str + " " + speed_str)
-
-
-# def Schedule(blocknum, blocksize, totalsize):
-#     speed = (blocknum * blocksize) / (time.time() - start_time)
-#     # speed_str = " Speed: %.2f" % speed
-#     speed_str = " Speed: %s" % format_size(speed)
-#     recv_size = blocknum * blocksize
-
-#     # 设置下载进度条
-#     f = sys.stdout
-#     pervent = recv_size / totalsize
-#     percent_str = "%.2f%%" % (pervent * 100)
-#     n = round(pervent * 50)
-#     s = ('#' * n).ljust(50, '-')
-#     print_diy(percent_str.ljust(6, ' ') + '-' + speed_str)
-#     f.flush()
-#     time.sleep(2)
 
 
 # 字节bytes转化K\M\G
@@ -176,7 +166,7 @@ def format_size(bytes):
 
 
 #  下载视频
-def down_video(video_list, title, start_url, page):
+def down_video(video_list, title, start_url, page, video_format="flv"):
     num = 1
     print_diy('[正在下载P{}段视频,请稍等...]:'.format(page) + title)
     currentVideoPath = os.path.join(OUTPUT_PATH, title)  # 当前目录作为下载目录
@@ -184,7 +174,7 @@ def down_video(video_list, title, start_url, page):
         opener = urllib.request.build_opener()
         # 请求头
         opener.addheaders = [
-            # ('Host', 'upos-hz-mirrorks3.acgvideo.com'),  #注意修改host,不用也行
+            # ('Host', 'upos-hz-mirrorks3.acgvideo.com'),  # 注意修改host,不用也行
             ('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:56.0) Gecko/20100101 Firefox/56.0'),
             ('Accept', '*/*'),
             ('Accept-Language', 'en-US,en;q=0.5'),
@@ -204,64 +194,48 @@ def down_video(video_list, title, start_url, page):
         else:
             reporthook_func = process_for_one_page
         if len(video_list) > 1:
-            urllib.request.urlretrieve(url=i, filename=os.path.join(currentVideoPath, r'{}-{}.flv'.format(title, num)),
+            urllib.request.urlretrieve(url=i, filename=os.path.join(currentVideoPath,
+                                                                    r'{}-{}.{}'.format(title, num, video_format)),
                                        reporthook=reporthook_func)
 
         else:
-            urllib.request.urlretrieve(url=i, filename=os.path.join(currentVideoPath, r'{}.flv'.format(title)),
+            urllib.request.urlretrieve(url=i,
+                                       filename=os.path.join(currentVideoPath, r'{}.{}'.format(title, video_format)),
                                        reporthook=reporthook_func)  # 写成mp4也行  title + '-' + num + '.flv'
         num += 1
 
 
-# 合并视频(20190802新版)
-# def combine_video(title_list):
-#     video_path = OUTPUT_PATH
-#     for title in title_list:
-#         current_video_path = os.path.join(video_path, title)
-#         if len(os.listdir(current_video_path)) >= 2:
-#             # 视频大于一段才要合并
-#             print_diy('[下载完成,正在合并视频...]:' + title)
-#             # 定义一个数组
-#             L = []
-#             # 遍历所有文件
-#             for file in sorted(os.listdir(current_video_path), key=lambda x: int(x[x.rindex("-") + 1:x.rindex(".")])):
-#                 # 如果后缀名为 .mp4/.flv
-#                 if os.path.splitext(file)[1] == '.flv':
-#                     # 拼接成完整路径
-#                     filePath = os.path.join(current_video_path, file)
-#                     # 载入视频
-#                     video = VideoFileClip(filePath)
-#                     # 添加到数组
-#                     L.append(video)
-#             # 拼接视频
-#             final_clip = concatenate_videoclips(L)
-#             # 生成目标视频文件
-#             final_clip.to_videofile(os.path.join(current_video_path, r'{}.mp4'.format(title)), fps=24,
-#                                     remove_temp=False)
-#             print_diy('[视频合并完成]' + title)
-#         else:
-#             # 视频只有一段则直接打印下载完成
-#             print_diy('[视频合并完成]:' + title)
+def start_a_thread(ths: list[threading.Thread]):
+    # 开始线程
+    for th in ths:
+        th.start()
+    # 等待所有线程运行完毕
+    for th in ths:
+        th.join()
 
 
-def do_prepare(inputStart, inputQuality):
+def do_prepare(inputStart, inputQuality, simultaneous_num: int = 5, video_format="flv"):
     global OUTPUT_PATH, total_amount
+    confirm['state'] = tk.DISABLED
     try:
         # 清空进度条
         download.coords(fill_line1, (0, 0, 0, 23))
         pct.set('0.00%')
         root.update()
+
         # 清空文本栏
-        msgbox.delete('1.0', 'end')
+        # msgbox.delete('1.0', 'end')
+
         start_time = time.time()
         # 用户输入av号或者视频链接地址
-        print_diy('*' * 30 + 'B站视频-下载小助手' + '*' * 30, print_time=False)
+        print_diy("\n\n" + '*' * 30 + 'B站视频-下载小助手' + '*' * 30, print_time=False)
         start = inputStart
         if bv_search := re.search(r'/?(BV\w+)/?', start):
             start_url = "https://api.bilibili.com/x/web-interface/view?bvid=" + bv_search.group(1)
         elif av_search := re.search(r'/(av\d+)/?', start):
             start_url = "https://api.bilibili.com/x/web-interface/view?aid=" + av_search.group(1)
         else:
+            initialize()
             raise Exception("解析错误。请检查输入的网址是否正确。")
 
         # 视频质量
@@ -306,29 +280,23 @@ def do_prepare(inputStart, inputQuality):
             start_url = start_url + "/?p=" + page
             video_list = get_play_list(start_url, cid, quality)
             start_time = time.time()
-            # down_video(video_list, title, start_url, page)
             # 定义线程
-            th = threading.Thread(target=down_video, args=(video_list, title, start_url, page))
+            th = threading.Thread(target=down_video, args=(video_list, title, start_url, page, video_format))
             # 将线程加入线程池
             threadpool.append(th)
-
-        # 开始线程
-        for th in threadpool:
-            th.start()
-        # 等待所有线程运行完毕
-        for th in threadpool:
-            th.join()
-
-        # 最后合并视频
-        # combine_video(title_list)
+        index_num = 0
+        thread_amount = len(threadpool)
+        while index_num < thread_amount:
+            if index_num + simultaneous_num >= thread_amount:
+                start_a_thread(threadpool[index_num:thread_amount])
+            else:
+                start_a_thread(threadpool[index_num:index_num + simultaneous_num])
+            index_num += simultaneous_num
 
         end_time = time.time()  # 结束时间
         print_diy('下载总耗时%.2f秒,约%.2f分钟' % (end_time - start_time, int(end_time - start_time) / 60))
 
         # 如果是windows系统，下载完成后打开下载目录
-        # if len(title_list) != 1:
-        #     currentVideoPath = os.path.join(OUTPUT_PATH, data['title'])
-
         if sys.platform.startswith('win'):
             os.startfile(OUTPUT_PATH)
     except Exception as e:
@@ -357,44 +325,61 @@ if __name__ == "__main__":
     # 各项输入栏和选择框
     inputStart = tk.Entry(root, bd=5, width=465, highlightcolor="#FF0000")
     labelStart = tk.Label(root, text="👇 请输入您要下载的B站视频链接:", font=12, justify=tk.CENTER, pady=10)
-
     labelStart.pack(anchor="w", )
     inputStart.pack()
-    labelQual = tk.Label(root, text="\n请选择您要下载视频的清晰度")  # 清晰度选择
+
+    labelQual = tk.Label(root, text="\n请选择您要下载视频的清晰度:")  # 清晰度选择
     labelQual.pack(anchor="w")
     inputQual = ttk.Combobox(root, state="readonly")
     # 可供选择的表
     inputQual['value'] = ('1080P', '720p', '480p', '360p')
-    # 对应的转换字典
-    keyTrans = dict()
+    keyTrans = dict()  # 对应的转换字典
     keyTrans['1080P'] = '80'
     keyTrans['720p'] = '64'
     keyTrans['480p'] = '32'
     keyTrans['360p'] = '16'
-    # 初始值为720p
-    inputQual.current(1)
+    inputQual.current(1)  # 初始值为720p
     inputQual.pack()
-    confirm = tk.Button(root, text="开始下载",
-                        command=lambda: thread_it(do_prepare, inputStart.get(), keyTrans[inputQual.get()]))
 
-    download = tk.Canvas(root, width=465, height=23, bg="white")
+    mp4_flv_input_label = tk.Label(root, text="\n请选择您要下载视频的格式:")  # 视频格式选择
+    mp4_flv_input_label.pack(anchor="w")
+    mp4_flv_input = ttk.Combobox(root, state="readonly")
+    mp4_flv_input['value'] = ('mp4', 'flv')
+    mp4_flv_input.current(0)
+    mp4_flv_input.pack()
+    mp4_flv_dict = {'mp4': "mp4", "flv": "flv"}
+
+    simultaneous_num_label = tk.Label(root, text="\n请选择同时下载的视频个数:")  # 视频格式选择
+    simultaneous_num_label.pack(anchor="w")
+    simultaneous_num_input = ttk.Combobox(root, state="readonly")
+    simultaneous_num_input['value'] = [str(i) for i in range(1, 10)]
+    simultaneous_num_input.current(4)
+    simultaneous_num_input.pack()
+
+    confirm = tk.Button(root, text="开始下载",
+                        command=lambda: thread_it(do_prepare, inputStart.get(),
+                                                  keyTrans[inputQual.get()],
+                                                  int(simultaneous_num_input.get()),
+                                                  mp4_flv_dict[mp4_flv_input.get()]))
+
+    download = tk.Canvas(root, width=465, height=15, bg="white")
     # 进度条的设置
     labelDownload = tk.Label(root, text="下载进度")
     labelDownload.pack(anchor="w")
     download.pack()
-    fill_line1 = download.create_rectangle(0, 0, 0, 23, width=0, fill="green")
+    fill_line1 = download.create_rectangle(0, 0, 0, 15, width=0, fill="green")
     pct = StringVar()
     pct.set('0.0%')
     pctLabel = tk.Label(root, textvariable=pct)
     pctLabel.pack()
-    root.geometry("700x600+400+100")
+    root.geometry("720x700+400+50")
     confirm.pack()
 
     msgbox = tk.Text(root, height=50, width=200)
     msgbox.insert('1.0',
-                  "💡 对于单P视频:直接传入视频链接地址\n例：https://www.bilibili.com/video/BV1UL411t7CR\n\n\n"
-                  "💡 对于多P视频:\n  1.下载全集:直接传入B站视频链接地址，例：https://www.bilibili.com/video/BV11K411376D\n"
-                  "  2.下载其中一集:传入那一集的视频链接地址，例：https://www.bilibili.com/video/BV11K411376D?p=8\n\n"
+                  "💡 对于单P视频:直接传入视频链接地址\n    例：https://www.bilibili.com/video/BV1UL411t7CR\n\n\n"
+                  "💡 对于多P视频:\n    1.下载全集:直接传入B站视频链接地址，例：https://www.bilibili.com/video/BV11K411376D\n"
+                  "    2.下载其中一集:传入那一集的视频链接地址，例：https://www.bilibili.com/video/BV11K411376D?p=8\n\n"
                   "该程序仅用于学习交流，请勿用于任何商业用途！谢谢\n")
 
     scroll = tk.Scrollbar()
@@ -403,12 +388,12 @@ if __name__ == "__main__":
     # 两个控件关联
     scroll.config(command=msgbox.yview)
     msgbox.config(yscrollcommand=scroll.set)
-
     msgbox.pack()
 
-    inputStart.insert(0, " ")
-    inputStart.focus()
+    inputStart.insert(0, " ")  # 美化输入框
+    inputStart.focus()  # 获取焦点
 
     normal_threading_num = len(threading.enumerate()) + 1
+
     # GUI主循环
     root.mainloop()
